41c059a26364a81bdfd4c79375acf57f324de76b
Furrycalin <Furrycalin@noreply.gitcode.com>
2025-03-02 14:48:52 +0800

    update: 更新文件 ChronixHubCommandLine.lua 同步部分内容
    Signed-off-by: Furrycalin <Furrycalin@noreply.gitcode.com>

--- ChronixHubCommandLine.lua
+++ ChronixHubCommandLine.lua
@@ -3,13 +3,20 @@ local UserInputService = game:GetService("UserInputService")
 local LocalPlayer = Players.LocalPlayer
 local playerGui = LocalPlayer:WaitForChild("PlayerGui")
 
+local CONFIG = {
+    lockSpeed = false,
+    Speed = LocalPlayer.Character.Humanoid.WalkSpeed,
+}
+
 -- 创建 ScreenGui
 local screenGui = Instance.new("ScreenGui")
-screenGui.Parent = playerGui
+screenGui.Parent = game.CoreGui
+screenGui.ResetOnSpawn = false
+screenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
 -- 创建日志框
 local logFrame = Instance.new("ScrollingFrame")
-logFrame.Size = UDim2.new(0.29, 0, 0.2, 0) -- 设置大小
+logFrame.Size = UDim2.new(0.29, 0, 0.3, 0) -- 设置大小
 logFrame.Position = UDim2.new(0.005, 0, 0.95, 0) -- 设置位置（聊天框上方）
 logFrame.AnchorPoint = Vector2.new(0, 1) -- 锚点左下角
 logFrame.BackgroundColor3 = Color3.new(0.2, 0.2, 0.2) -- 背景颜色
@@ -117,6 +124,10 @@ local function getPlayerByIdOrName(identifier)
     return nil -- 未找到玩家
 end
 
+game:GetService("RunService").Stepped:Connect(function()
+    if CONFIG.lockSpeed then LocalPlayer.Character.Humanoid.WalkSpeed = CONFIG.Speed end
+end)
+
 -- 处理指令
 local function handleCommand(commandParts)
     local command = commandParts[1] -- 第一个部分是指令
@@ -150,19 +161,111 @@ local function handleCommand(commandParts)
         else
             log("无效的参数数量！", "error")
         end
+    elseif command == "speed" then
+        if #commandParts == 2 then
+            LocalPlayer.Character.Humanoid.WalkSpeed = commandParts[2]
+            CONFIG.Speed = commandParts[2]
+            log("速度已更改为 " .. commandParts[2], "info")
+        else
+            log("无效的参数数量！", "error")
+        end
+    elseif command == "jump" then
+        if #commandParts == 2 then
+            LocalPlayer.Character.Humanoid.JumpPower = commandParts[2]
+            log("跳跃高度已更改为 " .. commandParts[2], "info")
+        else
+            log("无效的参数数量！", "error")
+        end
+    elseif command == "maxhealth" then
+        if #commandParts == 2 then
+            LocalPlayer.Character.Humanoid.MaxHealth = commandParts[2]
+            log("最大血量已更改为 " .. commandParts[2], "info")
+        else
+            log("无效的参数数量！", "error")
+        end
+    elseif command == "health" then
+        if #commandParts == 2 then
+            LocalPlayer.Character.Humanoid.Health = commandParts[2]
+            log("血量已更改为 " .. commandParts[2], "info")
+        else
+            log("无效的参数数量！", "error")
+        end
+    elseif command == "gravity" then
+        if #commandParts == 2 then
+            LocalPlayer.Character.Humanoid.Gravity = commandParts[2]
+            log("重力已更改为 " .. commandParts[2], "info")
+        else
+            log("无效的参数数量！", "error")
+        end
+    elseif command == "lockspeed" then
+        CONFIG.lockSpeed = not CONFIG.lockSpeed
+        log(CONFIG.lockSpeed and "速度已锁定" or "速度已解锁", "info")
+    elseif command == "antiafk" then
+        local bb=game:service'VirtualUser'
+        game:service'Players'.LocalPlayer.Idled:connect(function()bb:CaptureController()bb:ClickButton2(Vector2.new())end)
+        log("防挂机被踢开启", "info")
+    elseif command == "healthme" then
+        LocalPlayer.Character.Humanoid.Health = LocalPlayer.Character.Humanoid.MaxHealth 
+        log("已回满血(客户端)", "info")
+    elseif command == "killme" then
+        LocalPlayer.Character.Humanoid.Health = 0
+        log("已自杀", "info")
+    elseif command == "nightvision" then
+        if #commandParts == 2 then
+            if commandParts[2] == "true" then
+                game.Lighting.Ambient = Color3.new(1, 1, 1)
+                log("夜视开启", "info")
+            elseif commandParts[2] == "false" then
+                game.Lighting.Ambient = Color3.new(0, 0, 0)
+                log("夜视关闭", "info")
+            end
+        else
+            log("无效的参数数量！", "error")
+        end
+    elseif command == "gettptool" then
+        mouse = game.Players.LocalPlayer:GetMouse() tool = Instance.new("Tool") tool.RequiresHandle = false tool.Name = "手持点击传送" tool.Activated:connect(function() local pos = mouse.Hit+Vector3.new(0,2.5,0) pos = CFrame.new(pos.X,pos.Y,pos.Z) game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = pos end) tool.Parent = game.Players.LocalPlayer.Backpack
+        log("已添加点击传送工具", "info")
     elseif command == "log" then
         logFrame.Visible = not logFrame.Visible
         log(logFrame.Visible and "开启日志." or "关闭日志.", "info")
     elseif command == "help" then
-        log("-----------帮助页面-----------","info")
-        log("[]为可选参数, {}为必选参数.")
-        log("help - 进入帮助页面.")
-        log("log - 开启日志.")
-        log("exit - 卸载脚本")
-        log("tp {玩家名} - 传送到玩家.")
-        log("tp {玩家名1} {玩家名2} - 将玩家1传送到玩家2.")
-        log("tp {X} {Y} {Z} - 传送到指定坐标.")
-        log("------------------------------","info")
+        local function showhelp(page)
+            if page == nil or page == 1 then
+                log("-----------帮助页面(第1页/共3页)-----------","info")
+                log("[]为可选参数, {}为必选参数.")
+                log("help - 进入帮助页面.")
+                log("log - 开启日志.")
+                log("exit - 卸载脚本")
+                log("tp {玩家名} - 传送到玩家.")
+                log("tp {玩家名1} {玩家名2} - 将玩家1传送到玩家2.")
+                log("tp {X} {Y} {Z} - 传送到指定坐标.")
+                log("------------------------------------------","info")
+            elseif page == 2 then
+                log("-----------帮助页面(第2页/共3页)-----------","info")
+                log("speed {速度} - 更改移速.")
+                log("jump {跳跃高度} - 更改跳跃高度.")
+                log("maxhealth {最大血量} - 更改最大血量.")
+                log("health {血量} - 更改当前血量.")
+                log("gravity {重力} - 更改重力.")
+                log("lockspeed - 锁定移速.")
+                log("antiafk - 开启反挂机被踢.")
+                log("------------------------------------------","info")
+            elseif page == 3 then
+                log("-----------帮助页面(第3页/共3页)-----------","info")
+                log("healthme - 回满血.")
+                log("killme - 自杀.")
+                log("nightvision {true/false} - 开关夜视.")
+                log("gettptool - 获得点击传送工具.")
+                log("------------------------------------------","info")
+            else
+                log("错误的参数.", "error")
+            end
+        end
+        if #commandParts == 2 then
+            showhelp(tonumber(commandParts[2]))
+        else
+            showhelp()
+        end
     elseif command == "exit" then
         screenGui:Destroy() -- 销毁 ScreenGui
         script:Destroy() -- 卸载脚本
