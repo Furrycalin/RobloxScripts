ca9abdcb374fa7e842fba8b18d21737fb9ce5f26
Furrycalin <Furrycalin@163.com>
2025-03-13 11:35:14 +0800

    update: 更新文件 chat_test.lua
    Signed-off-by: Furrycalin <Furrycalin@163.com>

--- chat_test.lua
+++ chat_test.lua
@@ -17,14 +17,14 @@ function chatControl:chat(str)
 end
 
 -- 接收消息的函数
-function chatControl:MessageReceiver()
+function chatControl:MessageReceiver(callback)
     if not isLegacyChat then
         -- 新聊天系统：监听 RBXGeneral 频道的消息
         TextChatService.TextChannels.RBXGeneral.OnIncomingMessage:Connect(function(message)
             local messageData = {}
             messageData["sender"] = message.TextSource -- 发送者
             messageData["text"] = message.Text -- 消息内容
-            return messageData
+            callback(messageData)
         end)
     else
         -- 旧聊天系统：监听 OnMessageDoneFiltering 事件
@@ -32,7 +32,7 @@ function chatControl:MessageReceiver()
             local messageData = {}
             messageData["sender"] = messageData.FromSpeaker -- 发送者
             messageData["text"] = messageData.Message -- 消息内容
-            return messageData
+            callback(messageData)
         end)
     end
 end
