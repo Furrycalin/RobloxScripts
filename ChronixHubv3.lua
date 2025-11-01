472df4eacf03553dff834f9c924459769d417242
Furrycalin <Furrycalin@163.com>
2025-11-02 00:17:15 +0800

    update: 更新文件 ChronixHubv3.lua
    Signed-off-by: Furrycalin <Furrycalin@163.com>

---
+++ ChronixHubv3.lua
@@ -0,0 +1,50 @@
+if not game:IsLoaded() then
+	game.Loaded:Wait()
+end
+
+if _G.ChronixHubisLoaded then
+    warn("⛔ ChronixHub Already loaded! Please do not repeat the execution.")
+    return
+end
+
+_G.ChronixHubisLoaded = true
+
+local CoreGui = game:GetService("CoreGui")
+local UserInputService = game:GetService("UserInputService")
+local TweenService = game:GetService("TweenService")
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local RunService = game:GetService("RunService")
+local TweenService = game:GetService("TweenService")
+local SoundService = game:GetService("SoundService")
+local Lighting = game:GetService("Lighting")
+local MarketplaceService = game:GetService("MarketplaceService")
+local Workspace = game:GetService("Workspace")
+local player = Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+local humanoid = character:WaitForChild("Humanoid")
+local HumanoidRootPart = character:WaitForChild("HumanoidRootPart")
+local VirtualInputManager = game:GetService("VirtualInputManager")
+local StarterGui = game:GetService("StarterGui")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local TextChatService = game:GetService("TextChatService")
+local HttpService = game:GetService("HttpService")
+
+local LoadAnimationModule = loadstring(game:HttpGet("https://raw.gitcode.com/Furrycalin/RobloxScripts/raw/main/v3StartAnimation.lua"))()
+local tpWalk = loadstring(game:HttpGet("https://raw.gitcode.com/Furrycalin/RobloxScripts/raw/main/tpWalk.lua"))()
+
+local iscancel = false
+
+LoadAnimationModule:LoadAnimation(2, {
+    titleText = "ChronixHub V3",
+    loadingText = "加载中... ",
+    backgroundColor = Color3.new(0, 0, 0),
+    textColor = Color3.new(1, 1, 1),
+    language = "zh",
+    onComplete = function(isCancelled)
+        if isCancelled then
+            iscancel = true
+        end
+    end,
+    showCancelButton = true
+})
