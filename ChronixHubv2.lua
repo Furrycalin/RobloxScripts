705648a4d17124e0e6a18e54f9c787347e4aab10
Furrycalin <Furrycalin@163.com>
2026-02-11 23:17:58 +0800

    update: 更新文件 ChronixHubv2.lua
    Signed-off-by: Furrycalin <Furrycalin@163.com>

--- ChronixHubv2.lua
+++ ChronixHubv2.lua
@@ -41,6 +41,7 @@ local SpectatorModule = loadstring(game:HttpGet("https://raw.atomgit.com/Furryca
 local FreecamModule = loadstring(game:HttpGet("https://raw.atomgit.com/Furrycalin/ChronixHub/raw/main/modules/FreecamModule.lua"))()
 local LandingEffect = loadstring(game:HttpGet("https://raw.atomgit.com/Furrycalin/ChronixHub/raw/main/modules/LandingEffect.lua"))()
 local NameTagModule = loadstring(game:HttpGet("https://raw.atomgit.com/Furrycalin/ChronixHub/raw/main/modules/NameTagModule.lua"))()
+local PlayerVisibleModule = loadstring(game:HttpGet("https://raw.atomgit.com/Furrycalin/ChronixHub/raw/main/modules/PlayerVisibleModule.lua"))()
 
 local iscancel = false
 
@@ -1479,7 +1480,8 @@ local data = {
         antidead = false,
         floorY = nil,
         Spectator = false,
-        landingeffect = false
+        landingeffect = false,
+        visible = false
     },
     playercontrol = {
         lockspeed = false,
@@ -2712,6 +2714,11 @@ local function AddMenuContent(category)
         toolList.add("获得点击传送工具", function(button)
             mouse = game.Players.LocalPlayer:GetMouse() tool = Instance.new("Tool") tool.RequiresHandle = false tool.Name = "手持点击传送" tool.Activated:connect(function() local pos = mouse.Hit+Vector3.new(0,2.5,0) pos = CFrame.new(pos.X,pos.Y,pos.Z) game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = pos end) tool.Parent = game.Players.LocalPlayer.Backpack
         end)
+        toolList.add(data.tools.visible and "隐身(开)" or "隐身(关)", function(button)
+            data.tools.visible = not data.tools.visible
+            if data.tools.visible then PlayerVisibleModule.enable() else PlayerVisibleModule.disable() end
+            button.Text = data.tools.visible and "隐身(开)" or "隐身(关)"
+        end)
         toolList.add(data.tools.landingeffect and "落地特效(开)" or "落地特效(关)", function(button)
             data.tools.landingeffect = not data.tools.landingeffect
             if data.tools.landingeffect then LandingEffect.enable() else LandingEffect.disable() end
@@ -3261,6 +3268,7 @@ local function unloadchronixhub()
     data.musictest.enable = false
     data.tools.noclip = false
     data.tools.infjump = false
+    PlayerVisibleModule.unload()
     NameTagModule.unload()
     LandingEffect.unload()
     FreecamModule.unload()
