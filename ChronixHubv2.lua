ee977b517ce9951fa73de7fff1d68ad6a15b54e8
Furrycalin <Furrycalin@163.com>
2026-03-28 20:28:43 +0800

    update: 更新文件 ChronixHubv2.lua
    Signed-off-by: Furrycalin <Furrycalin@163.com>

--- ChronixHubv2.lua
+++ ChronixHubv2.lua
@@ -2978,10 +2978,10 @@ elseif GetDeviceType() == "Mobile" then
     fw.Position = UDim2.new(0.5, -40, 0.5, -40)
 end
 
-SystemNotification.Rainbow("ChronixHubV2已成功加载。\n欢迎使用！" .. displayName)
+SystemNotification.Rainbow("ChronixHubV2 Already Success Loaded!\nWelcome " .. displayName)
 
 local function unloadchronixhub()
-    SystemNotification.UnloadedGradient("ChronixHubv2已成功卸载。")
+    SystemNotification.UnloadedGradient("ChronixHubv2 Already Unload!")
     print("ChronixHubv2 已卸载。")
     _G.ChronixHubisLoaded = false
     data.musictest.enable = false
